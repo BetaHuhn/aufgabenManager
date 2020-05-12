51327b6acf0b6f7ee8b40d9171536fba7f98a9a6
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2020-05-12 15:40:14 +0200

    Changed session age from 48 hours to 7 days

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         secure: false,
-        maxAge: 48 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000, //7 Tage
         sameSite: "lax"
     },
     store: mongoStore
