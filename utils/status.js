9d2101632c4913c17fecedce174356620470d5e8
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2020-07-22 20:51:19 +0200

    Refactor backend

---
+++ utils/status.js
@@ -0,0 +1,13 @@
+module.exports = {
+    OK: 200,
+    REDIRECT: 300,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    NOT_ALLOWED: 405,
+    WRONG_DATA: 406,
+    CONFLICT: 409,
+    TOO_MANY_REQUESTS: 429,
+    SERVER_ERROR: 500,
+};
